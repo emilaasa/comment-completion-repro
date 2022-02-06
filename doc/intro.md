540b509c9c6a073decdd7b28f7f046cc07f527b5
Emil Aasa <emilaasa@gmail.com>
2022-02-06 22:28:50 +0100

    A fresh lein new was enough

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to comment-completion-repro
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/comment_completion_repro/core_test.clj
@@ -0,0 +1,7 @@
+(ns comment-completion-repro.core-test
+  (:require [clojure.test :refer :all]
+            [comment-completion-repro.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
